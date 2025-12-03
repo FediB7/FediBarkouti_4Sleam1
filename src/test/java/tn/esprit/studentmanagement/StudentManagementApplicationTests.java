1ea6c8ed124273dea620cdbead8014bea4727bb2
fedi <barkoutifedi15@gmail.com>
2025-12-03 14:20:15 +0100

    Test SonarQube quality fix16

--- src/test/java/tn/esprit/studentmanagement/StudentManagementApplicationTests.java
+++ src/test/java/tn/esprit/studentmanagement/StudentManagementApplicationTests.java
@@ -2,7 +2,7 @@ package tn.esprit.studentmanagement;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
+import static org.junit.jupiter.api.Assertions.assertTrue;
 @SpringBootTest
 class StudentManagementApplicationTests {
 
